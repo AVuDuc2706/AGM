407c094d04d3fcf2ab496667a798f25a1b65d0ee
AVuDuc <ducanhvu627@gmail.com>
2026-03-26 15:37:38 +0700

    swagger

--- AccountService/Program.cs
+++ AccountService/Program.cs
@@ -93,11 +93,14 @@ builder.Services.AddSwaggerGen(option =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseCors("AllowAngular");
 app.UseAuthentication();
